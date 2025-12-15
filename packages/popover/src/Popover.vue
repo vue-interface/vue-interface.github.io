2d1c80dd45837ae2dda554275cdb83d70c02667a
Andrew-Knt <jak@jandrewknight.com>
2025-12-15 16:51:48 -0500

    chore: added TW4 popover component

---
+++ packages/popover/src/Popover.vue
@@ -0,0 +1,129 @@
+<script setup lang="ts">
+import { arrow as arrowFn, autoUpdate, flip as flipFn, FlipOptions, offset as offsetFn, OffsetOptions, useFloating, UseFloatingOptions } from '@floating-ui/vue';
+import { computed, ref, ShallowRef, useTemplateRef } from 'vue';
+
+export type PopoverProps = {
+    arrow?: boolean;
+    placement?: UseFloatingOptions['placement'];
+    strategy?: UseFloatingOptions['strategy'];
+    middleware?: (arrow: Readonly<ShallowRef<HTMLDivElement | null>>) => UseFloatingOptions['middleware'];
+    flip?: FlipOptions;
+    offset?: OffsetOptions;
+};
+
+const props = withDefaults(defineProps<PopoverProps>(), {
+    arrow: true,
+    middleware: undefined,
+    placement: undefined,
+    strategy: undefined,
+    flip: undefined,
+    offset: () => ({
+        mainAxis: 10
+    } as OffsetOptions)
+});
+
+const triggerEl = useTemplateRef('triggerEl');
+const popoverEl = useTemplateRef('popoverEl');
+const arrowEl = useTemplateRef('arrowEl');
+const isOpen = ref(false);
+
+const { floatingStyles, middlewareData, placement } = useFloating(triggerEl, popoverEl, {
+    placement: props.placement,
+    middleware: props.middleware?.(arrowEl) ?? [
+        flipFn(props.flip),
+        offsetFn(props.offset),
+        arrowFn({
+            element: arrowEl
+        }),
+    ],
+    whileElementsMounted: autoUpdate
+});
+
+type Side = 'bottom' | 'left' | 'top' | 'right';
+
+const side = computed(() => placement.value.split('-')[0] as Side);
+
+const arrowPosition = computed(() => {
+    return {
+        top: 'bottom',
+        right: 'left',
+        bottom: 'top',
+        left: 'right'
+    }[side.value] as Side;
+});
+
+const arrowRotation = computed<Record<Side,string>>(() => ({
+    top: 'rotate(225deg)',
+    right: 'rotate(-45deg)',
+    bottom: 'rotate(45deg)',
+    left: 'rotate(135deg)',
+}));
+
+defineSlots<{
+    default?: () => void;
+    trigger?: (props: {
+        isOpen: boolean;
+        open: () => void;
+        close: () => void;
+        toggle: () => void
+    }) => void
+}>();
+
+function open() {
+    isOpen.value = true;
+}
+
+function close() {
+    isOpen.value = false;
+}
+
+function toggle() {
+    if(!isOpen.value) {
+        open();
+    }
+    else {
+        close();
+    }
+}
+</script>
+
+<template>
+    <div ref="triggerEl">
+        <slot
+            name="trigger"
+            v-bind="{ isOpen, open, close, toggle }" />
+    </div>
+    <div 
+        v-if="isOpen"
+        class="popover-container"
+        @click.self="close">
+        <div
+            ref="popoverEl"
+            role="tooltip"
+            data-popover
+            :style="floatingStyles"
+            class="popover">
+            <slot />
+            <div
+                v-if="arrow"
+                ref="arrowEl"
+                data-popover-arrow
+                class="popover-arrow"
+                :style="{
+                    transform: arrowRotation[arrowPosition],
+                    ...Object.assign({
+                        left:
+                            middlewareData.arrow?.x != null
+                                ? `${middlewareData.arrow.x}px`
+                                : '',
+                        top:
+                            middlewareData.arrow?.y != null
+                                ? `${middlewareData.arrow.y}px`
+                                : ''
+                    }, {
+                        [arrowPosition]: `calc(${-(arrowEl?.offsetWidth ?? 0) / 2}px - 1px)`
+                    })
+                }" />
+        </div>
+    </div>
+</template>
---
+++ packages/popover/vite.config.js
@@ -0,0 +1,47 @@
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { pascalCase } from 'change-case';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import pkg from './package.json';
+
+const fileName = pkg.name.split('/')[1];
+
+const external = [
+    ...(pkg.dependencies ? Object.keys(pkg.dependencies) : []),
+    ...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : [])
+];
+
+export default ({ command }) => defineConfig({
+    build: {
+        sourcemap: command === 'build',
+        lib: {
+            entry: path.resolve(__dirname, 'index.ts'),
+            name: pascalCase(fileName),
+            fileName,
+        },
+        rollupOptions: {
+            external,
+            output: {
+                globals: external.reduce((carry, dep) => {
+                    return Object.assign(carry, {
+                        [dep]: pascalCase(dep)
+                    });
+                }, {}),
+            }
+        },
+        watch: !process.env.NODE_ENV && {
+            include: [
+                './tailwindcss/**/*.js'
+            ]
+        }
+    },
+    plugins: [
+        vue(),
+        dts({
+            entryRoot: path.resolve(__dirname, './'),
+        }),
+        tailwindcss()
+    ],
+});
